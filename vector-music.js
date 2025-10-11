9d1339df674b5744ad31aa115ac9562d17f38fab
CaretParrot <rishi.venkatesh720@gmail.com>
2025-10-11 18:59:02 -0400

    Fix vector coordinates in Graph class and add MusicVector class

--- vector-music.js
+++ vector-music.js
@@ -7,6 +7,12 @@ const Graph = function (element, stroke = "white", strokeWidth = 5) {
     element.innerHTML += `<defs><marker id="arrow" markerWidth="20" markerHeight="20" refX="0" refY="2" orient="auto"><path d="M 0 0 L 4 2 L 0 4 z" fill="${this.stroke}" /></marker></defs>`;
 
     this.addVector = function (Vector) {
-        this.element.innerHTML += `<line x1=${Vector.getCoords()[0][0]} y1=${-Vector.getCoords()[0][1]} x2=${Vector.getCoords()[1][0]} y2=${Vector.getCoords()[1][1]} style="stroke: ${this.stroke}; stroke-width: ${this.strokeWidth}" marker-end="url(#arrow)" />`;
+        this.element.innerHTML += `<line x1=${Vector.getCoords()[0][0]} y1=${Vector.getCoords()[0][1]} x2=${Vector.getCoords()[1][0]} y2=${Vector.getCoords()[1][1]} style="stroke: ${this.stroke}; stroke-width: ${this.strokeWidth}" marker-end="url(#arrow)" />`;
     }
+}
+
+const MusicVector = function (frequencyIndex = 48, duration = 1, properties) {
+    this.frequencyIndex = frequencyIndex;
+    this.duration = duration;
+    this.properties = properties;
 }
