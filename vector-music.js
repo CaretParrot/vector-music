4929a10f42aef95820eeafe389cd0b6c1280c53b
CaretParrot <rishi.venkatesh720@gmail.com>
2025-10-25 09:49:12 -0400

    Update vector initialization in index.html and refactor addVector method in Graph class

--- vector-music.js
+++ vector-music.js
@@ -5,10 +5,10 @@ const Graph = function (element, stroke = "white", strokeWidth = 5) {
 
     element.setAttribute("transform", "scale(1, -1)");
 
-    this.addVector = function (vector) {
-        let newVector = vector;
-        if (vector instanceof PosVector) {
-            newVector = vector.getVector();
+    this.addVector = function (u) {
+        let newVector = u;
+        if (u instanceof PosVector) {
+            newVector = u.getVector();
         }
         this.element.innerHTML += `<line x1=${newVector.getCoords()[0][0]} y1=${newVector.getCoords()[0][1]} x2=${newVector.getCoords()[1][0]} y2=${newVector.getCoords()[1][1]} style="stroke: ${this.stroke}; stroke-width: ${this.strokeWidth}" marker-end="url(#arrow)" />`;
     }
