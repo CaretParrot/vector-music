4182acb4742c86f6db3ab22765e334d31f264c31
CaretParrot <rishi.venkatesh720@gmail.com>
2025-10-25 10:29:25 -0400

    Refactor vector handling in index.html and vector-music.js to improve SVG rendering and update logic

--- vector-music.js
+++ vector-music.js
@@ -1,16 +1,28 @@
-const Graph = function (element, stroke = "white", strokeWidth = 5) {
-    this.element = element;
-    this.stroke = stroke;
-    this.strokeWidth = strokeWidth;
+class Graph {
+    constructor(element, stroke = "white", strokeWidth = 5) {
+        this.element = element;
+        this.stroke = stroke;
+        this.strokeWidth = strokeWidth;
+        this.vectorList = [];
 
-    element.setAttribute("transform", "scale(1, -1)");
+        element.setAttribute("transform", "scale(1, -1)");
+        element.innerHTML += `<path d="M 0 0 L 4 2 L 0 4 z" fill="white" />`;
+    }
 
-    this.addVector = function (u) {
-        let newVector = u;
-        if (u instanceof PosVector) {
-            newVector = u.getVector();
+    setVectorList(list) {
+        this.vectorList = list;
+        this.update();
+    }
+
+    update() {
+        this.clear();
+        for (let i = 0; i < this.vectorList.length; i++) {
+            this.element.innerHTML += `<line x1=${this.vectorList[i].getCoords()[0][0]} y1=${this.vectorList[i].getCoords()[0][1]} x2=${this.vectorList[i].getCoords()[1][0]} y2=${this.vectorList[i].getCoords()[1][1]} style="stroke: ${this.stroke}; stroke-width: ${this.strokeWidth}" marker-end="url(#arrow)" />`;
         }
-        this.element.innerHTML += `<line x1=${newVector.getCoords()[0][0]} y1=${newVector.getCoords()[0][1]} x2=${newVector.getCoords()[1][0]} y2=${newVector.getCoords()[1][1]} style="stroke: ${this.stroke}; stroke-width: ${this.strokeWidth}" marker-end="url(#arrow)" />`;
+    }
+
+    clear() {
+        this.element.innerHTML = "";
     }
 }
 
